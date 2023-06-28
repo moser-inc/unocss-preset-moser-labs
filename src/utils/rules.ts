d5d2b407f4d6b556cfdabe7de8b4310b03341a7c
Daniel Waltz <daniel.waltz@moserit.com>
2023-06-28 13:50:04 -0400

    refactor: inline auto grid rule for easier copy paste

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -2,31 +2,31 @@ import { h } from '@unocss/preset-mini/utils';
 import { type RuleContext, type Rule } from 'unocss';
 import { type Theme } from 'unocss/preset-wind';
 
-const directions: Record<string, string> = {
-  cols: 'columns',
-  rows: 'rows',
-};
-
-const modes: Record<string, string> = {
-  fit: 'auto-fit',
-  fill: 'auto-fill',
-};
-
-function handleAutoGrid([, d, m, s]: string[], { theme }: RuleContext<Theme>) {
-  const v =
-    theme.width?.[s] ?? theme.spacing?.[s] ?? h.bracket.cssvar.global.rem(s);
-
-  if (v === null || v === undefined) return;
-
-  return {
-    [`grid-template-${directions[d]}`]: `repeat(${modes[m]}, minmax(min(100%, ${v}), 1fr))`,
-  };
-}
-
 export const moserLabsRules = [
   [
     /^grid-(cols|rows)-(fit|fill)-(.+)$/,
-    handleAutoGrid,
+    ([, d, m, s]: string[], { theme }: RuleContext<Theme>) => {
+      const directions: Record<string, string> = {
+        cols: 'columns',
+        rows: 'rows',
+      };
+
+      const modes: Record<string, string> = {
+        fit: 'auto-fit',
+        fill: 'auto-fill',
+      };
+
+      const wv = theme.width?.[s];
+      const sv = theme.spacing?.[s];
+      const fbv = h.bracket.cssvar.global.rem(s);
+      const v = wv ?? sv ?? fbv;
+
+      if (v === null || v === undefined) return;
+
+      return {
+        [`grid-template-${directions[d]}`]: `repeat(${modes[m]}, minmax(min(100%, ${v}), 1fr))`,
+      };
+    },
     {
       autocomplete: [
         'grid-(cols|rows)-(fit|fill)-$width',
