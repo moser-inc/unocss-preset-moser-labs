799e054b9f230430f8bad15ef365ffd7409306e0
Daniel Waltz <daniel.waltz@moserit.com>
2023-08-09 12:46:32 -0400

    feat: make labs nav sticky when scrolling

--- src/utils/shortcuts.ts
+++ src/utils/shortcuts.ts
@@ -15,13 +15,13 @@ export function moserLabsShortcuts(defaultApp?: MoserLabsAppThemeKey) {
       'labs-layout': 'h-viewport flex bg-card',
       'labs-layout-header': 'h-auto flex items-center gap-4 p-4 sm:h-20',
       'labs-layout-header-title': 'm-0 mr-auto text-8 text-primary-gradient',
-      'labs-layout-nav': 'flex shrink flex-col gap-2 p-3',
-      'labs-layout-nav-button': 'h-14! w-14!',
+      'labs-layout-nav':
+        'sticky top-0 h-full h-screen h-100dvh flex shrink flex-col gap-2 overflow-y-auto p-3 transition-all',
+      'labs-layout-nav-closed': 'min-w-20! w-20!',
+      'labs-layout-nav-open': 'min-w-62! w-62!',
+      'labs-layout-nav-button': 'min-h-14 min-w-14 h-14! w-14!',
       'labs-layout-nav-icon': 'i-scale-175 min-w-1em',
-      'labs-layout-nav-menu':
-        'shrink transition-width border-0! bg-transparent! py-0!',
-      'labs-layout-nav-menu-closed': 'w-14!',
-      'labs-layout-nav-menu-open': 'w-62!',
+      'labs-layout-nav-menu': 'shrink w-auto! border-0! bg-transparent! py-0!',
       'labs-layout-nav-menu-list': 'flex flex-col gap-2',
       'labs-layout-nav-menu-list-item':
         'h-14 gap-6 b-0! rounded-full! text-left!',
