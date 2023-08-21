73d686085a7e100483a7bcffcc1d6bb1c8c210da
Daniel Waltz <daniel.waltz@moserit.com>
2023-08-21 09:42:02 -0400

    fix: fix labs title shortcut line height rule value

--- src/utils/shortcuts.ts
+++ src/utils/shortcuts.ts
@@ -17,7 +17,7 @@ export function moserLabsShortcuts(defaultApp?: MoserLabsAppThemeKey) {
       'labs-layout-header-actions': '',
       'labs-layout-header-content': 'mr-auto flex items-end gap-2',
       'labs-layout-header-title':
-        'm-0 text-8 text-primary-gradient leading-slim',
+        'm-0 text-8 text-primary-gradient leading-tight',
       'labs-layout-header-subtitle': 'm-0 text-sm leading-loose',
       'labs-layout-nav':
         'sticky top-0 h-full h-screen h-100dvh flex shrink flex-col gap-2 overflow-y-auto p-3 transition-all',
