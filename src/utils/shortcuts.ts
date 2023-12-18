938ad70b803386730fe934984ca4270e9c9fac39
Daniel Waltz <daniel.waltz@moserit.com>
2023-12-18 11:05:41 -0500

    refactor: update important `!` order to match tailwind syntax style

--- src/utils/shortcuts.ts
+++ src/utils/shortcuts.ts
@@ -21,15 +21,15 @@ export function moserLabsShortcuts(defaultApp?: MoserLabsAppThemeKey) {
       'labs-layout-header-subtitle': 'm-0 text-sm leading-loose',
       'labs-layout-nav':
         'sticky top-0 h-full h-screen h-100dvh flex shrink flex-col gap-2 overflow-y-auto p-3 transition-all',
-      'labs-layout-nav-closed': 'min-w-20! w-20!',
-      'labs-layout-nav-open': 'min-w-62! w-62!',
-      'labs-layout-nav-button': 'min-h-14 min-w-14 h-14! w-14!',
+      'labs-layout-nav-closed': '!min-w-20 !w-20',
+      'labs-layout-nav-open': '!min-w-62 !w-62',
+      'labs-layout-nav-button': 'min-h-14 min-w-14 !h-14 !w-14',
       'labs-layout-nav-icon': 'i-scale-175 min-w-1em',
-      'labs-layout-nav-menu': 'shrink w-auto! border-0! bg-transparent! py-0!',
+      'labs-layout-nav-menu': 'shrink !w-auto !border-0 !bg-transparent !py-0',
       'labs-layout-nav-menu-list': 'flex flex-col gap-2',
       'labs-layout-nav-menu-list-item':
-        'h-14 gap-6 b-0! rounded-full! text-left!',
-      'labs-layout-nav-mobile': 'fixed! bottom-4 right-4 z-2',
+        'h-14 gap-6 !b-0 !rounded-full !text-left',
+      'labs-layout-nav-mobile': '!fixed bottom-4 right-4 z-2',
       'labs-layout-main': 'min-w-0 flex grow flex-col',
       'labs-layout-main-content':
         'relative flex grow flex-col overflow-hidden bg-ground pb-20 text-color sm:rounded-tl-3xl sm:pb-0',
