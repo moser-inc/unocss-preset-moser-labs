cc5d863da15bc0465b1b68dfffb464f738b65e2e
Daniel Waltz <daniel.waltz@moserit.com>
2025-01-13 10:36:04 -0500

    chore: use typescript for eslint config file

--- eslint.config.mjs
+++
@@ -1,3 +0,0 @@
-import moserConfig from '@moser-inc/eslint-config/flat';
-
-export default moserConfig({ tsconfigPath: 'tsconfig.json' });
---
+++ eslint.config.ts
@@ -0,0 +1,3 @@
+import moser from '@moser-inc/eslint-config/flat';
+
+export default moser({ tsconfigPath: 'tsconfig.json' });
